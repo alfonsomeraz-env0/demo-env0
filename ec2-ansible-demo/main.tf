0d0e63bfb91418089093cb16033fe632a297d955
Alfonso Meraz <alfonso.meraz@env0.com>
2026-04-15 15:33:14 -0400

    fix ansible setup

--- ec2-ansible-demo/main.tf
+++ ec2-ansible-demo/main.tf
@@ -6,7 +6,24 @@ resource "aws_instance" "demo" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t3.micro"
 
+  key_name = var.key_name
+
   tags = {
     Name = "env0-demo"
+  }
+}
+
+resource "null_resource" "trigger_ansible" {
+  depends_on = [aws_instance.demo]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      curl -s \
+        -H "Authorization: Bearer ${var.semaphore_token}" \
+        -H "Content-Type: application/json" \
+        -X POST \
+        -d '{"template_id": ${var.semaphore_template_id}, "environment": "{\"target_host\": \"${aws_instance.demo.public_ip}\"}" }' \
+        "${var.semaphore_url}/api/project/${var.semaphore_project_id}/tasks"
+    EOT
   }
 }
---
+++ ec2-ansible-demo/variables.tf
@@ -0,0 +1,5 @@
+variable "key_name"                {}
+variable "semaphore_url"           {}
+variable "semaphore_token"         { sensitive = true }
+variable "semaphore_template_id"   {}
+variable "semaphore_project_id"    {}
