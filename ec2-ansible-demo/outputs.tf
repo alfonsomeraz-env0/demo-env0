2ab003c58be7dc2f68abcea23005bbde8dfbbc0d
Alfonso Meraz <alfonso.meraz@env0.com>
2026-04-28 12:39:22 -0400

    fix ansible

--- ec2-ansible-demo/outputs.tf
+++ ec2-ansible-demo/outputs.tf
@@ -12,8 +12,3 @@ output "web_url" {
   description = "Load balancer URL"
   value       = "http://${aws_instance.web[0].public_ip}"
 }
-
-output "ssm_bucket_name" {
-  description = "S3 bucket used by Ansible for SSM file transfers"
-  value       = aws_s3_bucket.ansible_tmp.bucket
-}
